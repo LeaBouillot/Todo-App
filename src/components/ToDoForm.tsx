fd7e0af0935087974458eed462578f14db0c41f6
bestdevelope <leabouillot7@gmail.com>
2024-07-08 23:06:35 +0200

    button delete

--- src/components/ToDoForm.tsx
+++ src/components/ToDoForm.tsx
@@ -1,27 +1,12 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
-// Delete task
-const deleteTask = (index: number) => {
-  setTasks(tasks.filter((_, i) => i !== index));
-};
-
-// Start todit task
-const startEditTask = (index: number) => {
-  setEditIndex(index);
-  setEditText(tasks[index]);
-};
-
 function ToDoForm(props) {
   return (
     <div>
       <h1>To do List</h1>
       {props.todoList.map((item) => (
-        <TodoItem
-          item={item}
-          onDelete={() => deleteTask(index)}
-          onEdit={() => startEditTask(index)}
-        />
+        <TodoItem item={item} />
       ))}
     </div>
   );
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-function TodoItem(props) {
-  return <div>{props.item}</div>;
+function TodoItem(props, onDelete) {
+  return (
+    <div>
+      <span>{props.item}</span>
+      <button onClick={onDelete}>Delete</button>
+    </div>
+  );
 }
 
 export default TodoItem;
